67b9a680c7fbc1ec9e83a202cbdc405de0dc625e
David Ackroyd <davidackroydmusic@gmail.com>
2020-02-26 17:49:35 +0000

    elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -1,60 +1,64 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, div, input, text, p, h1, button)
+import Html exposing (Attribute, Html, button, div, h1, input, p, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
-
 import Note exposing (..)
+
 
 main =
     Browser.sandbox { init = init, update = update, view = view }
 
-type alias Model = 
-    {
-        newName : String,
-        newContent : String,
-        notes : List Note
+
+type alias Model =
+    { newName : String
+    , newContent : String
+    , notes : List Note
     }
+
 
 init : Model
 init =
-    {
-        newName = "",
-        newContent = "",
-        notes = []
+    { newName = ""
+    , newContent = ""
+    , notes = []
     }
 
-type Msg 
+
+type Msg
     = UpdateName String
     | UpdateContent String
     | AddNote
 
+
 update : Msg -> Model -> Model
-update msg model = 
+update msg model =
     case msg of
         AddNote ->
-            { model | notes = (Note model.newName model.newContent) :: model.notes}
+            { model | notes = Note model.newName model.newContent :: model.notes }
+
         UpdateName name ->
-            { model | newName = name}
+            { model | newName = name }
+
         UpdateContent content ->
-            { model | newContent = content}
+            { model | newContent = content }
+
 
 noteHtml : Note -> Html Msg
 noteHtml note =
     div []
-    [
-        p [] [text ("Name: " ++ (getName note))],
-        p [] [text ("Message: " ++ (getContent note))]
-    ]
+        [ p [] [ text ("Name: " ++ getName note) ]
+        , p [] [ text ("Message: " ++ getContent note) ]
+        ]
+
 
 view : Model -> Html Msg
-view model = 
+view model =
     div []
-    [
-        h1 [] [text "My Online Guestbook"],
-        input [placeholder "Your Name", onInput UpdateName] [],
-        input [placeholder "Your Message", onInput UpdateContent] [],
-        button [onClick AddNote] [text "Submit"],
-        div [] (List.map noteHtml model.notes)
-    ]
+        [ h1 [] [ text "My Online Guestbook" ]
+        , input [ placeholder "Your Name", onInput UpdateName ] []
+        , input [ placeholder "Your Message", onInput UpdateContent ] []
+        , button [ onClick AddNote ] [ text "Submit" ]
+        , div [] (List.map noteHtml model.notes)
+        ]
--- src/Note.elm
+++ src/Note.elm
@@ -2,16 +2,31 @@ module Note exposing (..)
 
 import Html exposing (Html, div, text)
 
-type alias Name = String
-type alias Content = String
-type Note 
-    = Note Name Content 
+
+type alias Name =
+    String
+
+
+type alias Content =
+    String
+
+
+type Note
+    = Note Name Content
+
 
 getContent : Note -> String
-getContent note = case note of
-    (Note _ c) -> c
+getContent note =
+    case note of
+        Note _ c ->
+            c
+
 
 getName : Note -> String
-getName note = case note of
-    (Note "" _) -> "Anonymous"
-    (Note n _) -> n
+getName note =
+    case note of
+        Note "" _ ->
+            "Anonymous"
+
+        Note n _ ->
+            n

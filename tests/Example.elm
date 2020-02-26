b2228be0fa9baca85161a85d116fc321f5b5d11f
David Ackroyd <davidackroydmusic@gmail.com>
2020-02-26 17:55:05 +0000

    removing todo

--- tests/Example.elm
+++ tests/Example.elm
@@ -5,6 +5,6 @@ import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
 
-suite : Test
-suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+-- suite : Test
+-- suite =
+--     todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
